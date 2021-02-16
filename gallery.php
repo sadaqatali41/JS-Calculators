48fc44ec1111cd92659b3d955e5bc45720003687
Sadaqat Ali <36379399+sadaqatali41@users.noreply.github.com>
2021-02-16 22:27:19 +0530

    file is updated

--- gallery.php
+++ gallery.php
@@ -1,25 +1,14 @@
 <?php
-include 'inc/config.php';
-include 'inc/func.php';
-include 'inc/pagination.php';
 include 'inc/resize-class.php';
-sec_session_start();
-if (login_check($conn) === FALSE) {
-	header("location: login.php");
-} else {
-	$userId = $_SESSION['user_id'];
-	$user = getLoginData($userId, $conn);
-}
 ?>
 <!DOCTYPE html>
 <html>
-<?php include 'inc/head.php';?>
+
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 <header class="main-header">
-    <?php include 'inc/header.php';?>
+    
 </header>
-<?php include 'inc/nav_sidebar.php';?>
 <div class="content-wrapper">
     <?php
 if (filter_input(INPUT_GET, "act") !== null) {
@@ -85,7 +74,7 @@ case "add":
 				$resizeObj->resizeImage(500, 300, "crop");
 				$resizeObj->saveImage("$trgtImgThumb/$newFileName", 100);
 
-				$stmt = $conn->prepare("INSERT INTO prod_gallery (prod_model, image) VALUES (?,?)");
+				$stmt = $conn->prepare("INSERT INTO table_name (prod_model, image) VALUES (?,?)");
 				$stmt->bind_param("is", $prod_model, $newFileName);
 				$stmt->execute();
 				print '<div class="alert alert-success alert-dismissable"><button class="close" type="button" data-dismiss="alert" aria-hidden="true">×</button>Success, product gallery is added successfully.</div>';
@@ -97,57 +86,6 @@ case "add":
 	}
 
 	?>
-    <section class="content-header">
-        <h1><i class="fa fa-bars"></i> Add Product Gallery</h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active"> Product Gallery</li>
-        </ol>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box box-info">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">All the fields marked with * are required</h3>
-                        <div class="pull-right">
-                            <a href="gallery.php" class="btn btn-sm btn-info"><i class='fa fa-plus'></i> View All</a>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <form role="form" action="gallery.php?act=add" method="post" enctype="multipart/form-data">
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="prod_model">Product Model</label>
-                                            <select name="prod_model" id="prod_model" class="form-control"></select>
-                                            <?php if (isset($error[0]) && !empty($error[0])) {print '<p class="text-danger">' . $error[0] . '</p>';}?>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="form-group col-md-10">
-                                            <label for="file">Image (max-size:1MB,min-width:1600px and min-height:900px)</label>
-                                            <input type="file" name="file" accept=".jpg, .png, .jpeg">
-                                            <?php if (isset($error[2]) && !empty($error[2])) {print '<p class="text-danger">' . $error[2] . '</p>';}?>
-                                            <?php if (isset($error[3]) && !empty($error[3])) {print '<p class="text-danger">' . $error[3] . '</p>';}?>
-                                            <?php if (isset($error[4]) && !empty($error[4])) {print '<p class="text-danger">' . $error[4] . '</p>';}?>
-                                            <?php if (isset($error[5]) && !empty($error[5])) {print '<p class="text-danger">' . $error[5] . '</p>';}?>
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <div id="bgPic"></div>
-                                        </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary" name="quality">Save</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     <?php
 break;
 case "edit":
@@ -163,7 +101,7 @@ case "edit":
 		$trgtIMG = "img/gallery";
 		$trgtImgThumb = "img/gallery/thumb";
 
-		$stmt = $conn->prepare("SELECT pg.*, pm.title FROM prod_gallery pg LEFT JOIN prod_model pm ON pg.prod_model=pm.id WHERE pg.id=?");
+		$stmt = $conn->prepare("SELECT pg.*, pm.title FROM table_name pg LEFT JOIN table_name1 pm ON pg.prod_model=pm.id WHERE pg.id=?");
 		$stmt->bind_param("i", $id);
 		$stmt->execute();
 		$resSm = $stmt->get_result();
@@ -226,23 +164,23 @@ case "edit":
 								@unlink("img/gallery/thumb/" . $event_pic);
 							}
 							$event_pic = $newFileName;
-							$stmt = $conn->prepare("UPDATE prod_gallery SET prod_model=?, image=? WHERE id=?");
+							$stmt = $conn->prepare("UPDATE table_name SET prod_model=?, image=? WHERE id=?");
 							$stmt->bind_param("isi", $prod_model, $newFileName, $id);
 							if ($stmt->execute()) {
 								print '<div class="alert alert-success alert-dismissable">
                                     <button class="close" type="button" data-dismiss="alert" aria-hidden="true">×</button>
-                                    Success, product gallery is updated successfully.
+                                    Success, xyz is updated successfully.
                                 </div>';
 							}
 						}
 					}
 				} else {
-					$stmt = $conn->prepare("UPDATE prod_gallery SET prod_model=? WHERE id=?");
+					$stmt = $conn->prepare("UPDATE table_name SET prod_model=? WHERE id=?");
 					$stmt->bind_param("ii", $prod_model, $id);
 					if ($stmt->execute()) {
 						print '<div class="alert alert-success alert-dismissable">
                                     <button class="close" type="button" data-dismiss="alert" aria-hidden="true">×</button>
-                                    Success, product model is updated successfully.
+                                    Success, xyz is updated successfully.
                                 </div>';
 					}
 				}
@@ -251,190 +189,17 @@ case "edit":
 	}
 
 	?>
-    <section class="content-header">
-        <h1><i class="fa fa-bars"></i> Edit Product Gallery</h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Edit Product Gallery</li>
-        </ol>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box box-info">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">All the fields marked with * are required</h3>
-                        <div class="pull-right">
-                            <a href="gallery.php" class="btn btn-sm btn-info"><i class='fa fa-list'></i> View All</a>
-                            <a href="gallery.php?act=add" class="btn btn-sm btn-info"><i class='fa fa-plus'></i> Add New</a>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <form role="form" action="gallery.php?act=edit&id=<?php print $id;?>" method="post" enctype="multipart/form-data">
-                                    <div class="row">
-                                        <div class="form-group col-md-4">
-                                            <label for="prod_model">Product Model*</label>
-                                            <select name="prod_model" id="prod_model" class="form-control">
-                                            	<option value="<?php echo $prod_model; ?>"><?php echo $title; ?></option>
-                                            </select>
-                                            <?php if (isset($error[0]) && !empty($error[0])) {print '<p class="text-danger">' . $error[0] . '</p>';}?>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="form-group col-md-6">
-                                            <label for="file">Image (max-size:1MB,min-width:1600px and min-height:900px)</label>
-                                            <input type="file" value="" name="file" placeholder="Event Image" accept=".jpg, .jpeg, png" />
-                                            <?php if (isset($error[2]) && !empty($error[2])) {print '<p class="text-danger">' . $error[2] . '</p>';}?>
-                                            <?php if (isset($error[3]) && !empty($error[3])) {print '<p class="text-danger">' . $error[3] . '</p>';}?>
-                                            <?php if (isset($error[4]) && !empty($error[4])) {print '<p class="text-danger">' . $error[4] . '</p>';}?>
-                                            <?php if (isset($error[5]) && !empty($error[5])) {print '<p class="text-danger">' . $error[5] . '</p>';}?>
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                            <div id="bgPic">
-                                                <?php if (isset($event_pic) && !empty($event_pic)) {print '<img style="max-width: 100px;" src="img/gallery/thumb/' . $event_pic . '">';}?>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary" name="editQuality" id="save">Save</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     <?php
 break;
 default:
 	?>
-    <section class="content-header">
-        <h1><i class="fa fa fa-bars fa-fw"></i>Manage Product Gallery</h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Manage Product Gallery</li>
-        </ol>
-    </section>
-    <section class="content">
-        <div class="row">
-            <div class="col-md-12 col-xs-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">List of all galleries</h3>
-                        <div class="pull-right">
-                            <a href="gallery.php?act=add" class="btn btn-sm btn-info"><i class='fa fa-plus'></i> Add New</a>
-                        </div>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body no-padding">
-                        <table class="table table-hover" id="example" style="width: 100%;">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Product Category</th>
-                                    <th>Product Name</th>
-                                    <th>Product Model</th>
-                                    <th>Image</th>
-                                    <th>Manage</th>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    
 <?php
 break;
 }
 ?>
     </div>
     <!-- /.content-wrapper -->
-    <?php include 'inc/footer.php';?>
 </div>
-<!-- ./wrapper -->
-<!-- jQuery 2.1.4 -->
-<script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-$.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.5 -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Slimscroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/app.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-<!-- Select2 -->
-<script src="plugins/select2/select2.full.min.js"></script>
-<script src="plugins/input-mask/jquery.inputmask.js"></script>
-<script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- datatables -->
-<script src="plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="plugins/datatables/dataTables.bootstrap.min.js"></script>
-
-<script type="text/javascript">
-
-	$("#prod_model").select2({
-		placeholder: "Search Product Model...",
-		width: '100%',
-		ajax: {
-			url: "ajax/handle-datatable.php",
-			type: 'POST',
-			dataType: 'json',
-			delay: 250,
-			data: function(params) {
-				return {
-					searchTerm: params.term, // search term
-					page: params.page,
-					act: "findProdModel"
-				};
-			},
-			processResults: function(data, params) {
-				params.page = params.page || 1;
-				return {
-					results: data.items,
-					pagination: {
-						more: (params.page * 10) < data.count_filtered
-					}
-				};
-			},
-			cache: false
-		}
-	});
-
-    $('#example').DataTable({
-        "processing": true,
-        "serverSide": true,
-        'serverMethod': 'post',
-
-        "ajax": {
-            "url": "ajax/handle-datatable.php",
-            "data": {"act": "galleryList"}
-        },
-        "columns": [
-            {"data": "id"},
-            {"data": "prod_cat"},
-            {"data": "prod"},
-            {"data": "prod_model"},
-            {"data": "image"},
-            {"data": "edit"}
-        ],
-        "columnDefs": [{
-            "targets": -1,
-            "orderable": false
-        }]
-});
-</script>
 </body>
 </html>
